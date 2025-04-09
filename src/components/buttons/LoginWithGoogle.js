2d39ae21ead42e132e549b5f1abeec8a5a96db46
CodeWithUmair <codewithumair867@gmail.com>
2025-04-10 04:43:02 +0500

    button sbg issues

--- src/components/buttons/LoginWithGoogle.js
+++ src/components/buttons/LoginWithGoogle.js
@@ -1,14 +1,15 @@
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { signIn } from "@/auth";
+"use client";
+
+import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react";
 
 export default function LoginWithGoogle() {
   return (
     <button
-      onClick={() => signIn("google")}
+      onClick={() => signIn("google")} // Correctly using the client-side signIn
       className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
     >
-      <FontAwesomeIcon icon={faGoogle} className="h-6" />
+      <FcGoogle size={20} />
       <span>Sign In with Google</span>
     </button>
   );
