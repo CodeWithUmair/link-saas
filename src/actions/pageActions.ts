5b10d4db080277ff9f3b60dd06e5f008e10f1f4f
CodeWithUmair <codewithumair867@gmail.com>
2025-04-09 06:41:47 +0500

    app pages added

--- src/actions/pageActions.ts
+++ src/actions/pageActions.ts
@@ -1,17 +1,17 @@
 'use server';
 
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
-import {Page} from "@/models/Page";
-import {User} from "@/models/User";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Page } from "@/models/Page";
+import { User } from "@/models/User";
 import mongoose from "mongoose";
-import {getServerSession} from "next-auth";
+import { getServerSession } from "next-auth";
 
-export async function savePageSettings(formData) {
-  mongoose.connect(process.env.MONGO_URI);
+export async function savePageSettings(formData: FormData) {
+  mongoose.connect(process.env.MONGO_URI!);
   const session = await getServerSession(authOptions);
   if (session) {
     const dataKeys = [
-      'displayName','location',
+      'displayName', 'location',
       'bio', 'bgType', 'bgColor', 'bgImage',
     ];
 
@@ -23,15 +23,15 @@ export async function savePageSettings(formData) {
     }
 
     await Page.updateOne(
-      {owner:session?.user?.email},
+      { owner: session?.user?.email },
       dataToUpdate,
     );
 
     if (formData.has('avatar')) {
       const avatarLink = formData.get('avatar');
       await User.updateOne(
-        {email: session.user?.email},
-        {image: avatarLink},
+        { email: session.user?.email },
+        { image: avatarLink },
       );
     }
 
@@ -41,17 +41,17 @@ export async function savePageSettings(formData) {
   return false;
 }
 
-export async function savePageButtons(formData) {
-  mongoose.connect(process.env.MONGO_URI);
+export async function savePageButtons(formData: FormData) {
+  mongoose.connect(process.env.MONGO_URI!);
   const session = await getServerSession(authOptions);
   if (session) {
     const buttonsValues = {};
     formData.forEach((value, key) => {
       buttonsValues[key] = value;
     });
-    const dataToUpdate = {buttons:buttonsValues};
+    const dataToUpdate = { buttons: buttonsValues };
     await Page.updateOne(
-      {owner:session?.user?.email},
+      { owner: session?.user?.email },
       dataToUpdate,
     );
     return true;
@@ -60,12 +60,12 @@ export async function savePageButtons(formData) {
 }
 
 export async function savePageLinks(links) {
-  mongoose.connect(process.env.MONGO_URI);
+  mongoose.connect(process.env.MONGO_URI!);
   const session = await getServerSession(authOptions);
   if (session) {
     await Page.updateOne(
-      {owner:session?.user?.email},
-      {links},
+      { owner: session?.user?.email },
+      { links },
     );
   } else {
     return false;
