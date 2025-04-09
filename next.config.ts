77a8a90f2f4910330cbed5e6c78e88e7f197cd39
CodeWithUmair <codewithumair867@gmail.com>
2025-04-09 06:04:06 +0500

    set nextConfig for new images allow in this app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: '*.googleusercontent.com'
+      },
+      {
+        hostname: 'linklist-files.s3.amazonaws.com',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
