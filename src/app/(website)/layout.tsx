d0b15b0148f2fbacef82e01e10411ddced30e97e
CodeWithUmair <codewithumair867@gmail.com>
2025-04-22 04:17:06 +0500

    footer added in landing page

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/layout/Header";
 import NextTopLoader from 'nextjs-toploader';
 import "../globals.css";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           showSpinner={false} />
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
