9bf9d07f4b2dc6949f4b3bc41eb6bf351d3180de
CodeWithUmair <codewithumair867@gmail.com>
2025-04-22 03:58:56 +0500

    hero section added

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,41 +1,10 @@
-import HeroForm from "@/components/forms/HeroForm";
-import { getServerSession, Session } from "next-auth";
+import Hero from '@/components/landing-page/Hero'
+import React from 'react'
 
-import Image from 'next/image';
-import Container from "@/components/layout/container";
-import { authOptions } from "../api/auth/[...nextauth]/authOptions";
-
-const Hero = async () => {
-  const session = (await getServerSession(authOptions)) as Session | null;
-
+export default function Page() {
   return (
-    <section className="w-full px-4 py-40 flex flex-col xl:flex-row items-center justify-center text-center bg-background">
-      <Container className="flex flex-col xl:flex-row items-center justify-between gap-4">
-        <div className="w-full text-left">
-          <h1 className="text-4xl sm:text-5xl font-bold mb-4 leading-tight">
-            Consolidate Your Social Links <br className="hidden sm:inline" />
-            Effortlessly
-          </h1>
-          <p className="text-gray-600 text-sm sm:text-base mb-8 max-w-2xl">
-            With our app, you can easily save and manage all your social links in one convenient location.
-            Track your online activity and share your profiles seamlessly across platforms.
-          </p>
-
-          <HeroForm user={session} />
-        </div>
-
-        <div className="w-full max-w-4xl aspect-auto bg-gray-200 rounded-xl overflow-hidden shadow-sm">
-          <Image
-            src="/images/hero.png"
-            alt="App Preview"
-            width={1200}
-            height={675}
-            className="object-cover w-full h-full rounded-lg"
-          />
-        </div>
-      </Container>
-    </section>
-  );
-};
-
-export default Hero;
+    <main className='min-h-screen'>
+      <Hero />
+    </main>
+  )
+}
