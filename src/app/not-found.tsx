4a016139e485f81e6c436fcf10e859b5644d5b65
CodeWithUmair <codewithumair867@gmail.com>
2025-04-25 07:27:06 +0500

    no found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/layout/Footer"
+import Header from "@/components/layout/Header"
+
+
+const NotFound = () => {
+    return (
+        <main className="min-h-screen">  <Header />
+            <div>Not Found</div>
+            <Footer />
+        </main>
+    )
+}
+
+export default NotFound
